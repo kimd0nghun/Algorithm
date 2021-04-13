28389a4fb60aa7d89a6b26ea17bd3692722d6d76
donghun <zhfptm12@naver.com>
2021-04-13 22:26:11 +0900

    gcd-lcm

---
+++ Algorithm/src/GCD_LCM/GCD_LCM.java
@@ -0,0 +1,19 @@
+package GCD_LCM;
+
+public class GCD_LCM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	static int gcd(int a, int b) {//when a<b
+		if (a % b == 0) {
+			return b;
+		} else
+			return gcd(b, a % b);
+	}
+
+	static int lcm(int a, int b) {
+		return (a * b) / gcd(b, a % b);
+	}
+}
